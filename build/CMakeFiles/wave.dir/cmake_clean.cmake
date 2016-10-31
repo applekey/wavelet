9d5e8f072aeddf1b2b886ec46069ad3f7f6eca08
vincent <applekey@gmail.com>
2016-10-31 16:20:11 -0700

    save

---
+++ build/CMakeFiles/wave.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/wave.dir/main.cpp.o"
+  "wave.pdb"
+  "wave"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/wave.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
